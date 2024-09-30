0c04ec7df68484a66000159f07ffeaca40b4d727
Anna N Helms <unicornbuddha@gmail.com>
2024-09-30 10:33:36 -0700

    Adds Footer section to App and builds it out. Adds Roadmap and builds out roadmap

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
-
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
@@ -16,6 +17,8 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
 
       <ButtonGradient />
